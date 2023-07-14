fa27a1933c50df1005cc499ebd6cfb16fa7d2bbd
jolie <jolieisaac@yahoo.com>
2023-07-14 16:50:23 -0700

    123

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 
 /**
- *
  * main - entry point
  * description: prints all alphabet letters
  *
  * return: always 0 (success)
-*/
+ */
 
 int main(void)
 {
 	char ch = 'a';
 	while (ch <= 'z')
+
 	{
 		putchar (ch);
 		ch++;
 	}
+
 	putchar('\n');
 	return (0);
 }

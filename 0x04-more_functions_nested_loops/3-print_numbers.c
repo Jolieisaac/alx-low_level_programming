f90052299983d83a24f9b42ca04fdafc25a1b1c0
jolie <jolieisaac@yahoo.com>
2023-07-21 14:24:56 -0700

    home

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints the numbers, from 0 to 9, followed by a new line
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void);
+{
+	char c;
+	for (c = 0; c < 10; c++)
+	{
+    _putchar(c);
+    	}
+	printf("\n")
+}

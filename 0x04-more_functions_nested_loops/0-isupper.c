ae85cb7e80f9a64b3bf7a1b210daba1f4a56cda6
jolie <jolieisaac@yahoo.com>
2023-07-21 14:10:33 -0700

    home

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include <main.h>
+#include <stdio.h>
+
+/**
+ * chech if c is uppercase
+ * return 0 or 1
+ */
+
+int _isupper(int c);
+	{
+	if (c >= 'A'; c <= 'Z')
+		return '1'
+	else 
+		return '0'
+	}
+
+
